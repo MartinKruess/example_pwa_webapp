98b90a0b78175f4751b6dbb5a82ec4c9a8805e56
MartinKruess <martin.kruss@digitalcareerinstitute.org>
2024-02-12 13:55:09 +0100

    pwa manifest

--- vite.config.js
+++ vite.config.js
@@ -5,16 +5,42 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   plugins: [
-    VitePWA({ pwa_manifest: {
-      icons: [
-        {
-          src: "./images/download-48.png",
-          sizes: "48x48",
-          type: "image/png",
-          purpose: "any maskable"
-        },
-      ]
-    }, registerType: 'autoUpdate' }),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      manifest: {
+        name: 'My Awesome App',
+        short_name: 'MyApp',
+        description: 'My Awesome App description',
+        theme_color: '#ffffff',
+        "icons": [
+          {
+            "src": "/pwa-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png",
+            "purpose": "any"
+          },
+          {
+            "src": "/pwa-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png",
+            "purpose": "any"
+          },
+          {
+            "src": "/pwa-maskable-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png",
+            "purpose": "maskable"
+          },
+          {
+            "src": "/pwa-maskable-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png",
+            "purpose": "maskable"
+          }
+        ],
+      }
+    }),
     react()
   ]
 })
